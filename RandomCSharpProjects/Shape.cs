f0fae801563c4d5365d0b73a0a9e5e3f75a93ccc
UnoriginalBapple <Basschan08@gmail.com>
2019-08-15 15:07:02 +0200

    OwO

---
+++ RandomCSharpProjects/Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomCSharpProjects
+{
+    class Shape
+    {
+        public string Type = "square";
+        public int Sides = 4;
+        public int Sidelength = 1;
+        public double area = 1;
+    }
+}
