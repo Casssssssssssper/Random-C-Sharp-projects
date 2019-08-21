f3cd41206427568119cf20da9047a5bac4aa4ded
UnoriginalBapple <Basschan08@gmail.com>
2019-08-21 15:11:02 +0200

    oof

---
+++ Code Snippets/TypeConversion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Snippets
+{
+    class TypeConversion
+    {
+        public double Distance { get; }
+
+        // Conversion operator for implicit conversion to kilometers public implicit operator Kilometers(Miles t)
+        public static implicit operator Kilometers(Miles t)
+        {
+            Console.WriteLine("implicit conversion from miles to kilometers");
+            return new Kilometers(t.Distance * 1.6);
+        }
+
+        public static explicit operator int(Miles t)
+        {
+            Console.WriteLine("implicit conversion from miles to kilometers");
+            return (int)(t.Distance + 0.5);
+        }
+
+        public Miles(double miles)
+        {
+            Distance = miles;
+        }
+
+        class Kilometers
+        {
+            public double Distance { get; }
+
+            public Kilometers(double kilometers)
+            {
+                Distance = Kilometers;
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Miles m = new Miles(100);
+
+                Kilometers k = m;
+
+                // More stuf fhere that is missing
+            }
+        }
+}
