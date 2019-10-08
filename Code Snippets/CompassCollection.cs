f8bf23374ab753049a40824b77e14a53490fc0cb
UnoriginalBapple <Basschan08@gmail.com>
2019-10-08 14:07:30 +0200

    Latest bug fixes

---
+++ Code Snippets/CompassCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Snippets
+{
+    class CompassCollection : ICollection
+    {
+        // Array containing values in this collection
+        string[] compassPoints = { };
+
+        public int Count => ((ICollection)compassPoints).Count;
+
+        public object SyncRoot => compassPoints.SyncRoot;
+
+        public bool IsSynchronized => compassPoints.IsSynchronized;
+
+        public void CopyTo(Array array, int index)
+        {
+            compassPoints.CopyTo(array, index);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return compassPoints.GetEnumerator();
+        }
+    }
+}
