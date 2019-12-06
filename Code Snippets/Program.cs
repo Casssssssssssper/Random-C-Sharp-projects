1f28d2187326d3ac1311944b0d4f86cc295b8c21
UnoriginalBapple <Basschan08@gmail.com>
2019-12-06 14:38:29 +0100

    Added code

--- Code Snippets/Program.cs
+++ Code Snippets/Program.cs
@@ -10,6 +10,19 @@ namespace Code_Snippets
     {
         static void Main(string[] args)
         {
-        }
+
+            // The following code uses the float type’s IsInfinity method to determine whether the narrowing conversion caused an overflow or underflow:
+
+            double big = -1E40;
+            float small = (float)big;
+            if (float.IsInfinity(small))
+                throw new OverflowException();
+
+             public string firstName;
+        public string lastname; public char initial; public double score1; public double score2; public double score3; public double score4; public double score5; public double average;
+        public void calcAverage() { double avg = ((score1 + score2 + score3 + score4 + score5) / 5); this.average = avg; }
+    }
+
+}
     }
 }
