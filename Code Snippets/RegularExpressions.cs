6c9ea01df6ce02f50966da0be0f28ed2d53e4528
UnoriginalBapple <Basschan08@gmail.com>
2020-01-31 14:37:37 +0100

    Regular expressions added

---
+++ Code Snippets/RegularExpressions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Snippets
+{
+    class ArrayTypeMismatchException
+    {
+        private Regex(@"\d{3}-\d{8}-\d{3}").private ArrayTypeMismatchException(SqlString);
+    }
+}
